8fed49c931a18217907ba1f26f09a5cc1227a99b
Hamzamt99 <tamarihamza4@gmail.com>
2023-06-09 00:30:51 +0000

    ..

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import './style.scss'
-import Hamza from '../../assets//Hamza.jpeg'
+import Hamza from '../../assets//lap.jpg'
 import { HashLink } from 'react-router-hash-link';
 function Main() {
   return (
     <div className='container'>
        <div className='contain-div'>
-      <h1>Hi, im <span className='hamza'>Hamza</span> a Software Engineer Based in Jordan</h1>
+      <h1>Hi, im <span className='pargraph'>Hamza</span> a Software Engineer and <span className='pargraph'>Full Stack</span> Developer Based in Jordan</h1>
       <Button className='btns'>Got Porject ? </Button>
       <Button  className='btns1'>Lets Talk</Button>
       <HashLink to='#home' className='hash'><i class="fa-solid fa-arrow-up"></i></HashLink>
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -10,24 +10,29 @@ export default function Services() {
             </div>
             <div className='cardRow'>
                 <div className='card'>
-                    <h1>Web Design</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-                    </p>
+                    <h2>Database Design and Management</h2>
+                    <p> I can assist in designing and implementing the database architecture for your application. This involves selecting an appropriate database management system
+                    (such as PostgreSQL) and creating efficient database schemas.</p>
                 </div>
                 <div className='card'>
-                    <h1>Web Development</h1>
-                    <p> I specialize in building websites from scratch or revamping existing ones. I use the latest web technologies and frameworks to create responsive, user-friendly, and scalable websites that align with your brand's identity and objectives.</p>
+                    <h2>Web Application Development</h2>
+                    <p> I can develop custom web applications from scratch using various technologies and frameworks such as React.js
+                        ,Node.js.
+                        I will ensure the application is scalable, responsive, and meets your specific requirements.
+                    </p>
                 </div>
             </div>
             <div className='cardRow'>
                 <div className='card'>
-                    <h1>UI/UX Design</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                    <h2>Front-end Development</h2>
+                    <p>I can create engaging and user-friendly interfaces using HTML, CSS, and JavaScript. I will focus on delivering a seamless user experience and ensure compatibility across different browsers and devices.
                     </p>
                 </div>
                 <div className='card'>
-                    <h1>Security</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    <h2>Back-end Development</h2>
+                    <p> I can build the server-side logic and database integration for your web application
+                    I will implement secure authentication, data storage, and efficient APIs to support your application's functionality.
+                    </p>
                 </div>
             </div>
             <Footer/>
